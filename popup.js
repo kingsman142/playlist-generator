6ea6d4dbedb9f780a98c754c011202405df14f3f
kingsman142 <kingsman142@yahoo.com>
2020-06-02 01:38:16 -0400

    #3 Added significant business logic in popup.js to locally store recentPlaylists, retrieve recent playlists, and create buttons for each playlist on popup creation

--- popup.js
+++ popup.js
@@ -5,12 +5,29 @@ James Hahn, 2016
 var bookmarkIds = []; // YouTube video IDs of each bookmark
 var folderNamesList = []; // names of folders where we should search for YouTube bookmarks
 var backgroundPage = chrome.extension.getBackgroundPage() // keep an active instance of the background page for easy logging
+var recentPlaylists = []; // a list of recent playlists requested by the user through the extension
 
 // main function to run the program -- find the YouTube video Ids of the bookmarks in the folders specified by the user
 function getBookmarkIds(shuffle = true){
     chrome.bookmarks.getTree(function(bookmarks){ // iterate over the bookmarks on the bookmarks bar
         var folderNamesString = window.document.getElementById("foldersForm").value;
-        parseFolders(folderNamesString);
+        folderNamesList = parseFolders(folderNamesString);
+
+        // update recentPlaylists in local storage
+        var folderNamesString = folderNamesList.toString();
+        console.log("folder names string: " + folderNamesString);
+        if(folderNamesList.length > 0){ // valid folder names
+            if(recentPlaylists.includes(folderNamesString)){ // if this list already exists in the previous 5 played playlists, pop it from the array so we can move it to the 'most recent' playlist position
+                var folderNamesIndex = recentPlaylists.indexOf(folderNamesString);
+                if(folderNamesIndex > -1) recentPlaylists.splice(folderNamesIndex, 1);
+            }
+            console.log(recentPlaylists);
+            recentPlaylists.unshift(folderNamesString);
+            console.log(recentPlaylists);
+            recentPlaylists = recentPlaylists.slice(0, 5);
+            console.log(recentPlaylists);
+            chrome.storage.sync.set({"recentPlaylists": recentPlaylists}, function(){ console.log("Added " + recentPlaylists[0] + " to recentPlaylists storage!"); })
+        }
 
         for(var j = 0; j < folderNamesList.length; j++){
             bookmarks.forEach(function(folder){
@@ -61,13 +78,49 @@ function findVideoID(url){
 
 // take the input of the folders from the HTML form and parse every folder name, which is separated by a comma.
 function parseFolders(names){
-    folderNamesList = names.split(","); // transform "Music, Music 2, Music 3" to ["Music", " Music 2", " Music 3"]
-    folderNamesList = folderNamesList.map(folderName => folderName.trim()); // trim leading/trailing whitespace i.e. transform ["Music", " Music 2", " Music 3"] to ["Music", "Music 2", "Music 3"]
-    backgroundPage.console.log(folderNamesList);
+    folderNames = names.split(","); // transform "Music, Music 2, Music 3" to ["Music", " Music 2", " Music 3"]
+    folderNames = folderNames.map(folderName => folderName.trim()); // trim leading/trailing whitespace i.e. transform ["Music", " Music 2", " Music 3"] to ["Music", "Music 2", "Music 3"]
+    backgroundPage.console.log(folderNames);
+    return folderNames;
+}
+
+function getRecentPlaylists(){
+    chrome.storage.sync.get(['recentPlaylists'],
+        function(returnDict){
+            console.log("Grabbed recent playlists:");
+            recentPlaylists = returnDict.recentPlaylists;
+            console.log(recentPlaylists);
+            console.log(" ");
+
+            // make modifications to the popup UI
+            var recentPlaylistsList = window.document.getElementById("recentPlaylistsList");
+            for(var i = 0; i < recentPlaylists.length; i++){
+                var recentsListItem = document.createElement("div");
+                recentsListItem.setAttribute("class", "recentsListItem");
+                var recentsInsertButton = document.createElement("div");
+                recentsInsertButton.setAttribute("class", "recentsInsertButton ph-button-insert ph-btn-white");
+                var recentsDeleteButton = document.createElement("div");
+                recentsDeleteButton.setAttribute("class", "recentsDeleteButton ph-button-delete ph-btn-red");
+
+                recentsInsertButton.appendChild(document.createTextNode(recentPlaylists[i]));
+                recentsDeleteButton.appendChild(document.createTextNode("X"));
+                recentsListItem.appendChild(recentsInsertButton);
+                recentsInsertButton.insertAdjacentElement('afterend', recentsDeleteButton);
+
+                recentPlaylistsList.appendChild(recentsListItem);
+            }
+            if(recentPlaylists.length == 0 || recentPlaylists === undefined){
+                console.log("EMPTY RECENT PLAYLISTS LIST!")
+                recentPlaylistsList.appendChild(document.createTextNode("No recent playlists"))
+            }
+        }
+    )
 }
 
 // calls the main program into action once the window loads and the user clicks the "Make playlist!" button
 window.onload = function(){
+    getRecentPlaylists();
+
     window.document.getElementById("bookmarksButtonShuffle").addEventListener('click', function(){ getBookmarkIds(shuffle = true); }, true);
     window.document.getElementById("bookmarksButtonInOrder").addEventListener('click', function(){ getBookmarkIds(shuffle = false); }, true);
 
